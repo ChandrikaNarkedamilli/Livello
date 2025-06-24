fc604a811a34b9cf6f458b17bfb2bc6cba629ee3
ChandrikaNarkedamilli <chandrikanarkedamilli07@gmail.com>
2025-06-24 15:40:10 +0530

    updated axios.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const app = express();
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cors({
   origin: function (origin, callback) {
+    console.log("CORS Request from:", origin); 
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
