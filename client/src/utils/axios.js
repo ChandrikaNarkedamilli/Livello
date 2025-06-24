3961d04ea8c5dbebe2ddc5fcea75b12b3f80bb65
ChandrikaNarkedamilli <chandrikanarkedamilli07@gmail.com>
2025-06-24 15:54:14 +0530

    updated url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+export const baseURL = "https://livello.onrender.com";
+
 const instance = axios.create({
-  baseURL  : "https://livello.onrender.com/api",
+  baseURL: `${baseURL}/api`,
   withCredentials: true,
-})
+});
 
 export default instance;
